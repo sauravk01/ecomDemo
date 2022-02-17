dd526731db4c9882ce5e04ac915ed4b331c7d642
sauravk01 <khanalsaurav655@gmail.com>
2022-02-17 19:49:16 +0545

    Update link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import Link from "next/link";
 const index = ({ data }) => {
   const { data: session } = useSession();
   console.log(session);
@@ -15,20 +16,59 @@ const index = ({ data }) => {
       </button>
     </div>
   ) : (
-    <div>
-      {session.user.name}
-      {session.user.email}
-      {session.user.image}
-      {data}
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          signOut();
-        }}
-      >
-        SignOut
-      </button>
-    </div>
+    <>
+      <div>
+        {session.user.name}
+        {session.user.email}
+        {session.user.image}
+        {data}
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            signOut();
+          }}
+        >
+          SignOut
+        </button>
+      </div>
+      <div>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/create/category">
+            <a>Create Category</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/create/color">
+            <a>Create Color</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/create/product">
+            <a>Create product</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/create/sub-category">
+            <a>Create sub category</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/products">
+            <a>Products</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/cart">
+            <a>Cart</a>
+          </Link>
+        </li>
+      </div>
+    </>
   );
 };
 
