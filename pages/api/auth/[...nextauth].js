714a349f54169aa9a81976b3def4aa80d0055130
sauravk01 <khanalsaurav655@gmail.com>
2022-02-17 18:15:05 +0545

    adding env files

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export default NextAuth({
   // Configure one or more authentication providers
